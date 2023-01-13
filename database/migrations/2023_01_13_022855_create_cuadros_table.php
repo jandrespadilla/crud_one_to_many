d4bc95d3df26fe40afdeb1941f9cce4e17b895e5
Jorge Andres Padilla <jandrespadilla@gmail.com>
2023-01-13 13:55:01 -0300

    Corrijo category_id

--- database/migrations/2023_01_13_022855_create_cuadros_table.php
+++ database/migrations/2023_01_13_022855_create_cuadros_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('nombre');
             $table->string('descripcion');
-            $table->foreignId('categori_id')
+            $table->foreignId('category_id')
                     ->nullable()
                     ->constrained('categories')
                     ->cascadeOnUpdate()

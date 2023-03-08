67e428161f6c22a11f3f36f6bf5fdba344e7e8eb
Jorge Andres Padilla <jandrespadilla@gmail.com>
2023-03-08 10:07:54 -0300

    subir

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,18 @@ use Illuminate\Support\Facades\Route;
 Route::resource('category',CategoryController::class)
             ->only(['index','show', 'update','store','destroy']);   
 
-Route::resource('cuadros',CuadrosController::class)
+/**/Route::resource('cuadros',CuadrosController::class)
             ->only(['index','show', 'update','store','destroy']);   
 
 /*Route::resource('register',AuthController::class)
             ->only(['register','login', 'userProfile','logout','allUser']);   
 */
+
+
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 Route::group(['middleware' => ['auth:sanctum']],function(){
     Route::get('user-profile',[AuthController::class,'userProfile']);
+   
     Route::post('logout',[AuthController::class,'logout']);
 });
